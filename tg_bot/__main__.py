6f116280b7e5bf51a372ae0786d12d0cab7780e2
LEGEND X <74249238+legendx22@users.noreply.github.com>
2020-12-27 19:54:05 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -146,7 +146,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                                                                                    [InlineKeyboardButton(text="ADD PIKACHU TO YOUR GROUP",
                                                                        url="t.me/{}?startgroup=true".format(bot.username)),
                                                                                    InlineKeyboardButton(text="Source Code",
-                                                                       url="https://github.com/sirinivasop/PIKACHU-OP")
+                                                                       url="https://github.com/srinivasop/PIKACHU-OP")
                                                                                  ]]))
 
     else:
