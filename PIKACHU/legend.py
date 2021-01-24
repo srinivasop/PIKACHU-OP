c47563ae1c4df594f4a1e46c4672172d0c894a1e
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-01-24 16:59:08 +0530

    @LEGENDXOP @LEGENDX22 IS BEST

---
+++ PIKACHU/legend.py
@@ -0,0 +1,11 @@
+#maked by LEGENDX22 OR LEGENDXOP
+#Made by TEAM LEGEND
+import os
+demon = os.environ.get("DEMONS")
+tigers = os.environ.get("TIGERS")
+dragon = os.environ.get("DRAGONS")
+wolves = os.environ.get("WOLVES")
+dev = os.environ.get("DEV_USERS")
+owner = os.environ.get("OWNER_USERNAME")
+
+creator = "LEGEND X"
