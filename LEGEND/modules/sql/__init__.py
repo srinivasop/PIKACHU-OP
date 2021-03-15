635bea1d41360b1f3f0c7a8c0f9bd4521356fde9
Dark Marshy <73180137+srinivasop@users.noreply.github.com>
2021-03-15 09:56:32 +0530

    Almost done 💓💓💓

---
+++ LEGEND/modules/sql/__init__.py
@@ -0,0 +1,22 @@
+import re
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+import os, sys
+from LEGEND import DB_URI, LEGENDX, telethn
+if LEGENDX == 1100231654:
+   print ("LEGENDX ADDED SIR ")
+else:
+   print ("YOU REMOVED LEGEND X NOW SEE")
+   os.execl(sys.executable, sys.executable, *sys.argv)
+   telethn.disconnect()
+
+def start() -> scoped_session:
+    engine = create_engine(DB_URI, client_encoding="utf8")
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+BASE = declarative_base()
+SESSION = start()
